edb4553c746b971e47a41eb5cd0e2bb4194ae680
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-03-10 23:26:40 +0200

    changed grid layout, center grid container, fixed card overflow

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// const apiRoot = "https://pokeapi.co/api/v2";
-
-// window.addEventListener("load", (event) => {
-//   async () => {
-//     const url = apiRoot + "/pokemon";
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     console.log(data);
-//   };
-// });
-
 // Queries
 let allPokemonContainer = document.querySelector(".card-container");
 
@@ -17,18 +6,14 @@ const fetchData = () => {
   fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
     .then((response) => response.json())
     .then((data) => {
-      //   console.log("fetch result", data);
-      //   pokeCards(data.results);
       const fetches = data.results.map((dataItem) => {
         return fetch(dataItem.url).then((res) => res.json());
       });
       Promise.all(fetches).then((res) => {
         pokeCards(res);
-        console.log(res, "res");
         let pokeType = res.forEach((resItem) =>
           resItem.types.forEach((resItemType) => {
             let eachPokeType = resItemType.type.name;
-            // console.log(eachPokeType);
             return eachPokeType;
           })
         );
@@ -47,8 +32,8 @@ const pokeCards = (dataFromFetch) => {
         <img src="${dataItem.sprites.other.dream_world.front_default}" alt="" />
       </div>
       <div class="poke-details">
-          <h1 class="poke-name">${dataItem.name}</h1>
-          <span class="poke-type">${types}</span>
+          <h4 class="poke-name">${dataItem.name}</h4>
+          <span class="poke-type">Type: ${types}</span>
       </div>
     </div>`;
     })

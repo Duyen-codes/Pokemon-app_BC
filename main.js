6cda96b397b714b0dffb67ed891a59b5ee6edc9d
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-03-10 23:56:02 +0200

    experiment with js

--- main.js
+++ main.js
@@ -1,44 +1,46 @@
 // Queries
 let allPokemonContainer = document.querySelector(".card-container");
 
-// Fetch data
-const fetchData = () => {
-  fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
-    .then((response) => response.json())
-    .then((data) => {
-      const fetches = data.results.map((dataItem) => {
-        return fetch(dataItem.url).then((res) => res.json());
-      });
-      Promise.all(fetches).then((res) => {
-        pokeCards(res);
-        let pokeType = res.forEach((resItem) =>
-          resItem.types.forEach((resItemType) => {
-            let eachPokeType = resItemType.type.name;
-            return eachPokeType;
-          })
-        );
-      });
-    });
-};
+// const fetchData = () => {
+//   fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const fetches = data.results.map((dataItem) => {
+//         return fetch(dataItem.url).then((res) => res.json());
+//       });
+//       Promise.all(fetches).then((res) => {
+//         pokeCards(res);
+//         let pokeType = res.forEach((resItem) =>
+//           resItem.types.forEach((resItemType) => {
+//             let eachPokeType = resItemType.type.name;
+//             return eachPokeType;
+//           })
+//         );
+//       });
+//     });
+// };
 
-// Use data
-const pokeCards = (dataFromFetch) => {
-  const cards = dataFromFetch
-    .map((dataItem) => {
-      let types = dataItem.types.map((item) => item.type.name).join(", ");
-      console.log(types);
-      return `<div class="card">
-      <div class="poke-img">
-        <img src="${dataItem.sprites.other.dream_world.front_default}" alt="" />
-      </div>
-      <div class="poke-details">
-          <h4 class="poke-name">${dataItem.name}</h4>
-          <span class="poke-type">Type: ${types}</span>
-      </div>
-    </div>`;
-    })
-    .join("");
-  allPokemonContainer.innerHTML = cards;
-};
+// const pokeCards = (dataFromFetch) => {
+//   const cards = dataFromFetch
+//     .map((dataItem) => {
+//       let types = dataItem.types.map((item) => item.type.name).join(", ");
+//       console.log(types);
+//       return `<div class="card">
+//       <div class="poke-img">
+//         <img src="${dataItem.sprites.other.dream_world.front_default}" alt="" />
+//       </div>
+//       <div class="poke-details">
+//           <h4 class="poke-name">${dataItem.name}</h4>
+//           <span class="poke-type">Type: ${types}</span>
+//       </div>
+//     </div>`;
+//     })
+//     .join("");
+//   allPokemonContainer.innerHTML = cards;
+// };
 
-fetchData();
+// fetchData();
+
+const url = "https://pokeapi.co/api/v2/pokemon/pokemon";
+
+fetch(url).then((res) => res.json());
